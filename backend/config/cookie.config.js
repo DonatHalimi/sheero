74de613f3d99745bad77d229a8b741e655c299a2
Donat <donat.halimi03@gmail.com>
2024-12-01 23:55:31 +0100

    2nd try to fix cookie config

--- backend/config/cookie.config.js
+++ backend/config/cookie.config.js
@@ -1,10 +1,10 @@
 const cookieConfig = {
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: '/',
-    domain: process.env.NODE_ENV === 'production' ? 'sheero.onrender.com' : 'localhost'
+    domain: process.env.NODE_ENV === 'production' ? '.onrender.com' : 'localhost',
 };
 
 module.exports = cookieConfig;
